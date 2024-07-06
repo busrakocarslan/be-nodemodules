4a096ef9cb6c8f382b39a1709616cb7d9e3c47f1
busra <kocarslanbusrab@gmail.com>
2024-07-06 07:33:24 +0300

    exercise middleware

--- middleware.js
+++ middleware.js
@@ -45,32 +45,91 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------- */
 //! middleware-router arası mesaj iletme
 //TODO req ve res ile bunu yapabiliriz.
-app.get("/", (req, res, next) => {
-  console.log("middleware çalıştı.");
-  // Bir sonraki controller'a data gönderme:
-  //REq
-  // req.message = 'Hello world from middleware BÜŞRA'
-  //res
-  if (req.query.courseName == "clarusway") {
-    res.message = "doğru yer";
-  } else {
-    res.send({
-      message: "ACABA?",
-    });
-  }
+// app.get("/", (req, res, next) => {
+//   console.log("middleware çalıştı.");
+//   // Bir sonraki controller'a data gönderme:
+//   //REq
+//   // req.message = 'Hello world from middleware BÜŞRA'
+//   //res
+//   if (req.query.courseName == "clarusway") {
+//     res.message = "doğru yer";
+//   } else {
+//     res.send({
+//       message: "ACABA?",
+//     });
+//   }
 
-  next();
-});
+//   next();
+// });
 
-// Route-Path:
-app.get("/", (req, res) => {
-  console.log("route-path çalıştı.");
-  res.send({
-    // message: req.message,
-    message: res.message,
-  });
-});
+//* Route-Path:
+// app.get("/", (req, res) => {
+//   console.log("route-path çalıştı.");
+//   res.send({
+//     // message: req.message,
+//     message: res.message,
+//   });
+// });
 
 /* ------------------------------------------------------- */
+//? yazdığımız aynı zamanda bir fonk olduğundan ayrıca da yazabiliyoruz.Ancak dikkat edilecek husus app'dan bunun haberdar olması için use.app()ile bildirmemiz gerektiğidir.
+// app.get('/',(req, res, next) => {
+//   console.log('middleware-1 çalıştı.')
+//   next()
+// })
+
+// const middleware1=(req, res, next) => {
+//   console.log('middleware-1 fonksiyonu çalıştı.')
+//   next()
+// }
+// const middleware2=(req, res, next) => {
+//   console.log('middleware-2 fonksiyonu çalıştı.')
+//   next()
+// }
+// app.use([middleware2,middleware1])// tek use ile , koyup yazaviliriz fonk sadece dikkat et BURAYA önce yazdığın  önce çelışır.
+//?URL e özel Middlewares
+// app.use('/test', [middleware1, middleware2])
+
+// app.get('/*',middleware1,middleware2, (req, res) => {
+// res.send({
+// message: 'Sorun yok.',
+// })
+// })
+
+// app.get('/', (req, res) => {
+// res.send({
+// message: 'Sorun yok  middleware geçti.',
+// })
+// })
+/* ------------------------------------------------------- */
+//TODO___ AYRI DOSYADAN ÇAĞIRMA
+
+
+const middlewares=require('./middlewares/')// orada arrayde export ettiğimden burada böyle çaığırabildim. Hepsine birden ihtiycacın varsa middlewarelerin array içinde yap 
+// const {middleware1,middleware2,middleware3}=require('./middlewares/')// orada obje export ettiğimden destruc ile burada böyle çaığırabildim. bazılarına ihtiyacın var ise obj içinde yap.
+
+
+app.get('/', middlewares.middleware1, (req, res) => {
+  res.send({
+  message: 'Sorun yok  middleware diğer klasörden geldi',
+  })
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,3 +3,36 @@
     EXPRESSJS - MIDDLEWARES
 ------------------------------------------------------- */
 
+// const middleware1=(req, res, next) => {
+//     console.log('middleware-1 fonksiyonu çalıştı.')
+//     next()
+//   }
+//   const middleware2=(req, res, next) => {
+//     console.log('middleware-2 fonksiyonu çalıştı.')
+//     next()
+//   }
+//   const middleware3=(req, res, next) => {
+//     console.log('middleware-3 fonksiyonu çalıştı.')
+//     next()
+//   }
+  //*ARRAY İÇİNDE EXPORT
+//   module.exports=[middleware1,middleware2,middleware3]
+  //*OBJE İÇİNDE EXPORT
+//   module.exports={middleware1,middleware2,middleware3}
+
+//TODO__ hepsini direk obj içinde tanımlayıp export etme
+module.exports={
+     middleware1:(req, res, next) => {
+        console.log('middleware-1 fonksiyonu çalıştı.')
+        next()
+      },
+      middleware2:(req, res, next) => {
+        console.log('middleware-2 fonksiyonu çalıştı.')
+        next()
+      },
+      middleware3:(req, res, next) => {
+        console.log('middleware-3 fonksiyonu çalıştı.')
+        next()
+      }
+
+}
